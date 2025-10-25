6c1f92eecfb9d6aec046e286aed5efe5490ce435
wawanher487 <wawanher487@gmail.com>
2025-10-25 09:24:27 +0700

    fix deploy

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,7 @@
 export const API_URL = import.meta.env.VITE_API_BASE_URL;
 
 // base URL untuk gambar produk (tanpa /api)
-export const IMAGE_URL_PRODUCT = "http://localhost:5000/uploads/products";
+export const IMAGE_URL_PRODUCT = "https://e-commerce-api-ykmv.onrender.com/uploads/products";
 
 // base URL untuk gambar produk (tanpa /api)
-export const IMAGE_URL_USER = "http://localhost:5000/uploads/users";
+export const IMAGE_URL_USER = "https://e-commerce-api-ykmv.onrender.com/uploads/users";
